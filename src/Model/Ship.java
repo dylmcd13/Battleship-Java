3fcd55d30c93ae23beedf343feca15bab9852c26
dylmcd13 <dylmcd13@gmail.com>
2022-09-18 22:12:56 -0400

    Finished Ship Class

--- src/Model/Ship.java
+++ src/Model/Ship.java
@@ -2,17 +2,61 @@ package Model;
 
 public class Ship {
 	
+  /** If ship is destroyed */
 	private boolean isDestroyed;
-	private int lengthOfShip; //length
+
+  /** length of ship */
+	private int lengthOfShip;
+  
+  /** Name of Ship */
 	private String shipName;
 	
+  /** Default Constructor for Ship Class */
 	public Ship() {
 		isDestroyed = false;
 		lengthOfShip = 0;
 		shipName = "";
 	}
 	
+  /**
+   * @return True or False if ship is destroyed
+   */
 	public boolean getIsDestroyed() {
 		return isDestroyed;
 	}
+
+  /**
+   * @return The length of ship
+   */
+  public int getLengthOfShip() {
+    return lengthOfShip;
+  }
+
+  /**
+   * @return Name of ship
+   */
+  public String getShipName() {
+    return shipName;
+  }  
+
+  /**
+   * Set isDestroyed to true or false
+   * @param isDestroyed : If ship is destroyed
+   */
+  public void setIsDestroyed(boolean isDestroyed){
+    this.isDestroyed = isDestroyed;
+  }
+
+  /**
+   * Set length of ship
+   * @param lengthOfShip : length of ship
+   */
+  public void setLengthOfShip(int lengthOfShip) {
+    this.lengthOfShip = lengthOfShip;
+  }
+
+
+  public void setNameOfShip(String shipName) {
+    this.shipName = shipName;
+  }
 }
