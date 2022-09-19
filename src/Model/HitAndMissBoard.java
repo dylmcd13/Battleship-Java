5bdd3dcee2a73032ea4ac458968c3a68d4f5b622
dylmcd13 <dylmcd13@gmail.com>
2022-09-19 10:52:22 -0400

    WIP of HitAndMissBoard

---
+++ src/Model/HitAndMissBoard.java
@@ -0,0 +1,23 @@
+package Model;
+
+public class HitAndMissBoard extends Board {
+  private Coordinate[] hitLocations;
+
+
+  private Coordinate[] missLocations;
+
+
+  public Coordinate[] getHitLocations() {
+    return hitLocations;
+  }
+
+
+  public Coordinate[] getMissLocations() {
+    return missLocations;
+  }
+
+
+  public void setHitLocation(Coordinate c){
+    
+  }
+}
