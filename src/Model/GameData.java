5ac771a0b39de670f4beceaeae91bbd0e03b619f
Alex Hearne <47182454+alexhearne@users.noreply.github.com>
2022-10-11 14:50:16 -0400

    Add files via upload
    Game Data

---
+++ src/Model/GameData.java
@@ -0,0 +1,24 @@
+import java.util.Observer;
+import java.util.Observable;
+
+public class GameData {
+    Ship ships;
+    Pegs peg;
+    boolean playerTurn;
+
+    public getHit()
+    {
+        return peg;            
+    }   
+
+    public getShips()
+    {
+        return ships;
+    }
+
+    public getPlayerTurn()
+    {
+        return playerTurn;
+    }
+
+}
