36e244c624eb4f531670df27ea30ad0828da4c65
Alex Hearne <47182454+alexhearne@users.noreply.github.com>
2022-10-11 15:17:55 -0400

    Add files via upload
    Controller

---
+++ src/View/BattleController.java
@@ -0,0 +1,24 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.event.*;
+
+
+public class BattleController implements MouseMotionListener{
+    GameData data;
+    BattleshipView view;
+
+    BattleController ()
+    {
+
+    }
+
+    public void mouseDragged(mouseEvent md)
+    {
+        
+    }
+
+    public boolean mouseMoved(mouseEvent mm)
+    {
+
+    }
+}
