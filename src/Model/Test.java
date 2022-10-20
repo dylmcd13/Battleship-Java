a7109c989a35474e62f3d12ad5ec39c86ec89f44
dylmcd13 <dylmcd13@gmail.com>
2022-10-20 10:28:09 -0400

    Added test class, prolly wont be used

---
+++ src/Model/Test.java
@@ -0,0 +1,13 @@
+package Model;
+class Test {
+  
+  public static void main(String[] args){
+    Board b = new Board();
+    
+    /*
+    Space[][] spaces = b.getGrid();
+    Coordinate c = spaces[1][0].getCoordinate();
+    System.out.println(c.getY());
+    */
+  }
+}
